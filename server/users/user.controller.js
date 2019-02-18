582bcfa8a50ef6eb612c2ab731cfba22d94d488a
Peter Kirkham <>
2019-02-18 14:13:01 +0000

    basic auth server - no functionality yet

---
+++ server/users/user.controller.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const userService = require('./user.service');
+
+// routes
+router.post('/authenticate', authenticate);
+router.get('/', (req, res, next) => res.json({}));
+
+module.exports = router;
+
+function authenticate(req, res, next) {
+    userService.authenticate(req.body)
+        .then(user => user ? res.json(user) : res.status(400).json({ message: 'Username or password is incorrect' }))
+        .catch(err => next(err));
+}
---
+++ server/users/user.service.js
@@ -0,0 +1,21 @@
+const config = require('config.json');
+const jwt = require('jsonwebtoken');
+
+// users hardcoded for simplicity, store in a db for production applications
+const users = [{ id: 1, username: 'test', password: 'test' }];
+
+module.exports = {
+    authenticate
+};
+
+async function authenticate({ username, password }) {
+    const user = users.find(u => u.username === username && u.password === password);
+    if (user) {
+        const token = jwt.sign({ sub: user.id }, config.secret);
+        const { password, ...userWithoutPassword } = user;
+        return {
+            ...userWithoutPassword,
+            token
+        };
+    }
+}
